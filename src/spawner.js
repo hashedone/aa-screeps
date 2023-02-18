bf0cf83b82c24c313cab89c9c88f54ec34be3bfc
Bartłomiej Kuras <hash.deb@gmail.com>
2023-02-18 23:42:09 +0100

    Attach spawners to room object

--- src/spawner.js
+++ src/spawner.js
@@ -2,6 +2,16 @@
 //
 // It accepts orders for new spawns, and prioritizes their order basing on room needs.
 
+// Enqueues and order. Body is requested body, name is the name prefix (doesn't have to be uniqueue - it will
+// be suffixed with spawn name + id), memory is the memory to initialize it with.
+function order(body, name, memory) {
+    this.memory.queue.push({
+        body: body,
+        name: name,
+        memory: memory
+    })
+}
+
 // Create spawner for the room
 function spawner(room) {
     if(room.memory.spawner == undefined) {
@@ -15,12 +25,17 @@ function spawner(room) {
                 spawn => spawn.id,
             ),
             queue: [],
+            id: 0,
         };
     }
 
-    return {
-        memory: room.memory.spawner
-    }
+    const spawner = {
+        memory: room.memory.spawner,
+        order: order
+    };
+
+    room.spawner = spawner;
+    return spawner;
 }
 
 // Create spawners for all owned rooms
