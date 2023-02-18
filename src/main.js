9a24cd170f7642dc32aee4c0f990a8d828b2fa2d
Bartłomiej Kuras <hash.deb@gmail.com>
2023-02-18 18:48:25 +0100

    Fixes

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
 const spawner = require('spawner');
-const miner = require('miner');
+// const miner = require('miner');
 
 function spawn_creeps() {
-    const spawners = spawner.spawners();
-
-    for(const idx in Game.spawns) {
-        const spawn = Game.spawns[idx];
-        const max_cost = spawn.room.energyCapacityAvailable;
-
-        const miner_body = miner.body(max_cost);
-
-        const mem_next_id = spawn.memory.next_id;
-        const next_id = (mem_next_id ? mem_next_id : 0)
-        const id = spawn.name + '_' + next_id;
-
-        if(spawn.spawnCreep(miner_body, miner.name(id)) == OK) {
-            spawn.memory.next_id = next_id + 1;
-        }
-    }
+//    const spawners = spawner.spawners();
+//
+//    for(const idx in Game.spawns) {
+//        const spawn = Game.spawns[idx];
+//        const max_cost = spawn.room.energyCapacityAvailable;
+//
+//        const miner_body = miner.body(max_cost);
+//
+//        const mem_next_id = spawn.memory.next_id;
+//        const next_id = (mem_next_id ? mem_next_id : 0)
+//        const id = spawn.name + '_' + next_id;
+//
+//        if(spawn.spawnCreep(miner_body, miner.name(id)) == OK) {
+//            spawn.memory.next_id = next_id + 1;
+//        }
+//    }
 }
 
 module.exports.loop = () => {
